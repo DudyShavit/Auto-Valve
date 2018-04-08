65e5b18e2e769f7f3ab3de7b6cf001bfe1c37a75
Dudy Shavit <shavit.dudy@gmail.com>
2018-04-08 23:24:37 +0300

    from some unknown reason: need to changed the order of functions in order to compile (for daylight hour purposes) no other change made

--- AUTO_VALVE.ino
+++ AUTO_VALVE.ino
@@ -78,6 +78,141 @@ void pulseCounter()
 }
 
 
+
+
+  void OpenValve()
+  {
+    Serial.println("Open_Valve");
+    digitalWrite(enablePin, HIGH);
+    digitalWrite(motorTerminal1, HIGH); // these logic levels create reverse direction
+    digitalWrite(motorTerminal2, LOW); 
+    //delayMicroseconds(900);
+    delay(80);
+    digitalWrite(enablePin, LOW);
+  }
+  void CloseValve()
+  {
+    Serial.println("Close_Valve");
+    digitalWrite(enablePin, HIGH);
+    digitalWrite(motorTerminal1, LOW); //these logic levels create forward direction
+    digitalWrite(motorTerminal2, HIGH); 
+    delay(120);
+    digitalWrite(enablePin, LOW);   
+  }
+
+  
+
+  void PrintCurrentTime(DateTime now)
+  {
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    
+  }
+
+
+void InitFlowRateSensor()
+{
+    pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
+
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+
+  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
+  // Configured to trigger on a FALLING state change (transition from HIGH
+  // state to LOW state)
+  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+}
+int PrintFlowRateMeasure()
+{
+  
+ unsigned int frac;
+    
+    // Print the flow rate for this second in litres / minute
+    Serial.print("Flow rate: ");
+    Serial.print(int(flowRate));  // Print the integer part of the variable
+    Serial.print(".");             // Print the decimal point
+    // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
+    frac = (flowRate - int(flowRate)) * 10;
+    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
+    Serial.print("L/min");
+    // Print the number of litres flowed in this second
+    Serial.print("  Current Liquid Flowing: ");             // Output separator
+    Serial.print(flowMilliLitres);
+    Serial.print("mL/Sec");
+
+    // Print the cumulative total of litres flowed since starting
+    Serial.print("  Output Liquid Quantity: ");             // Output separator
+    Serial.print(totalMilliLitres);
+    Serial.println("mL");
+
+     return flowMilliLitres;
+}
+
+int MeasureFlowRate()
+{
+   if((millis() - oldTime) > 1000)    // Only process counters once per second
+  { 
+    // Disable the interrupt while calculating flow rate and sending the value to
+    // the host
+    detachInterrupt(sensorInterrupt);
+        
+    // Because this loop may not complete in exactly 1 second intervals we calculate
+    // the number of milliseconds that have passed since the last execution and use
+    // that to scale the output. We also apply the calibrationFactor to scale the output
+    // based on the number of pulses per second per units of measure (litres/minute in
+    // this case) coming from the sensor.
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+    
+    // Note the time this processing pass was executed. Note that because we've
+    // disabled interrupts the millis() function won't actually be incrementing right
+    // at this point, but it will still return the value it was set to just before
+    // interrupts went away.
+    oldTime = millis();
+    
+    // Divide the flow rate in litres/minute by 60 to determine how many litres have
+    // passed through the sensor in this 1 second interval, then multiply by 1000 to
+    // convert to millilitres.
+    flowMilliLitres = (flowRate / 60) * 1000;
+    
+    // Add the millilitres passed in this second to the cumulative total
+    totalMilliLitres += flowMilliLitres;
+      
+    int currentflow = PrintFlowRateMeasure();
+
+    // Reset the pulse counter so we can start incrementing again
+    pulseCount = 0;
+    
+    // Enable the interrupt again now that we've finished sending output
+    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+
+    return currentflow;
+  }
+}
+
+void SyncSoftwareRTC()
+{
+    DateTime now = rtc_1307.now();
+    PrintCurrentTime(now);
+    rtc_millis.begin(now);
+    PrintCurrentTime(rtc_millis.now());    
+}
+  
 void setup() 
 {  
   Serial.begin(38400);
@@ -87,7 +222,7 @@ void setup()
       while (1);
    }
 
-   if (! rtc_1307.isrunning()) 
+   if (!rtc_1307.isrunning()) 
    {
       Serial.println("RTC is NOT running!");
       // following line sets the RTC to the date & time this sketch was compiled
@@ -255,135 +390,4 @@ void loop() {
 
   }
 }   
-
-
-  void OpenValve()
-  {
-    Serial.println("Open_Valve");
-    digitalWrite(enablePin, HIGH);
-    digitalWrite(motorTerminal1, HIGH); // these logic levels create reverse direction
-    digitalWrite(motorTerminal2, LOW); 
-    //delayMicroseconds(900);
-    delay(80);
-    digitalWrite(enablePin, LOW);
-  }
-  void CloseValve()
-  {
-    Serial.println("Close_Valve");
-    digitalWrite(enablePin, HIGH);
-    digitalWrite(motorTerminal1, LOW); //these logic levels create forward direction
-    digitalWrite(motorTerminal2, HIGH); 
-    delay(120);
-    digitalWrite(enablePin, LOW);   
-  }
-
-  void SyncSoftwareRTC()
-  {
-    DateTime now = rtc_1307.now();
-    PrintCurrentTime(now);
-    rtc_millis.begin(now);
-    PrintCurrentTime(rtc_millis.now());    
-  }
-
-  void PrintCurrentTime(DateTime now)
-  {
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    
-  }
-
-
-void InitFlowRateSensor()
-{
-    pinMode(sensorPin, INPUT);
-  digitalWrite(sensorPin, HIGH);
-
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
-
-  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
-  // Configured to trigger on a FALLING state change (transition from HIGH
-  // state to LOW state)
-  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-}
-int MeasureFlowRate()
-{
-   if((millis() - oldTime) > 1000)    // Only process counters once per second
-  { 
-    // Disable the interrupt while calculating flow rate and sending the value to
-    // the host
-    detachInterrupt(sensorInterrupt);
-        
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    
-    // Note the time this processing pass was executed. Note that because we've
-    // disabled interrupts the millis() function won't actually be incrementing right
-    // at this point, but it will still return the value it was set to just before
-    // interrupts went away.
-    oldTime = millis();
-    
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
-    flowMilliLitres = (flowRate / 60) * 1000;
-    
-    // Add the millilitres passed in this second to the cumulative total
-    totalMilliLitres += flowMilliLitres;
-      
-    int currentflow = PrintFlowRateMeasure();
-
-    // Reset the pulse counter so we can start incrementing again
-    pulseCount = 0;
-    
-    // Enable the interrupt again now that we've finished sending output
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-
-    return currentflow;
-  }
-}
-int PrintFlowRateMeasure()
-{
-  
- unsigned int frac;
-    
-    // Print the flow rate for this second in litres / minute
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print(".");             // Print the decimal point
-    // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
-    frac = (flowRate - int(flowRate)) * 10;
-    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-    Serial.print("L/min");
-    // Print the number of litres flowed in this second
-    Serial.print("  Current Liquid Flowing: ");             // Output separator
-    Serial.print(flowMilliLitres);
-    Serial.print("mL/Sec");
-
-    // Print the cumulative total of litres flowed since starting
-    Serial.print("  Output Liquid Quantity: ");             // Output separator
-    Serial.print(totalMilliLitres);
-    Serial.println("mL");
-
-     return flowMilliLitres;
-}
   
